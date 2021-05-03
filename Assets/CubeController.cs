be6e89608d345df911d60eef2f0019a8d3a4ab98
Yuterfrw771 <75987761+Yuterfrw771@users.noreply.github.com>
2021-05-03 11:01:16 +0900

    Initial commit

---
+++ Assets/CubeController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    AudioSource audioSource2D;
+    //キューブの移動速度
+    private float speed = -12;
+
+    //消滅位置
+    private float deadline = -10;
+
+    //SEの取得
+    public AudioClip sound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //AudioSourceによる再生
+        this.audioSource2D = GetComponent<AudioSource>();
+        this.audioSource2D.volume = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+
+        //画面外に出たら破棄する
+        if(transform.position.x < this.deadline)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        //Playclipによる再生
+        //AudioSource.PlayClipAtPoint(sound, this.transform.position, 1);
+
+        if (collision.gameObject.name == "UnityChan2D")
+        {
+           
+            this.audioSource2D.volume = 0;
+            //AudioSource.PlayClipAtPoint(sound, this.transform.position, 0);
+        }
+        else
+        {
+            this.audioSource2D.volume = 1;
+        }
+    }
+}
